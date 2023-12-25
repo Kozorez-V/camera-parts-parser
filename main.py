ad8755124a9360352c82b45f5b17e1aab15aa6c6
Kozorez-V <kozorezviktoria@gmail.com>
2023-12-25 16:28:06 +0400

    validation

--- main.py
+++ main.py
@@ -18,28 +18,47 @@ camera_brands = {
         "https://in-commerce.ru/zapchasty/dlya-zerkalnyh-fotoapparatov/spare-parts-pana-penta-fuji.html"
 }
 
+# get url
+
 url.show_camera_brand_list(camera_brands)
 
 brand_number = url.get_brand_number()
 
 page_url = url.get_page_url(camera_brands, brand_number)
 
+# parse the data
+
 while True:
+
+    # get response
     response = call.response(page_url)
 
+    # scrape the web page
     data = scraper.get_data(response)
-
     item_list = scraper.get_items(data)
 
+    # convert the data to DataFrame
     item_data = converter.get_item_data(item_list)
 
+    # go to the next page
     next_page_url = scraper.get_next_page(data)
 
     if next_page_url:
         page_url = urljoin(page_url, next_page_url)
     else:
+        # check if a folder exist; if not, then create it
         if not os.path.isdir('./export'):
             os.makedirs('./export')
+
+        # choose the file format
         format_number = export.choose_format()
-        export.export(item_data, format_number)
+        result_check_format_number = export.check_format_number(format_number)
+        export.show_result_check_format_number(result_check_format_number, format_number)
+
+        # get file name
+        file_name = export.get_file_name()
+        export.check_file_name(file_name)
+
+        # export
+        export.export(item_data, format_number, file_name)
         break
--- modules/export.py
+++ modules/export.py
@@ -1,3 +1,6 @@
+import os
+
+
 def choose_format():
     format_number = int(input("Please, select a format to export the data by entering its number:"
                               "\n1. CSV"
@@ -7,13 +10,43 @@ def choose_format():
     return format_number
 
 
-def export(item_data, format_number):
-    if format_number == 1:
-        item_data.to_csv('./export/data.csv', index=False)
-    if format_number == 2:
-        item_data.to_excel('./export/data.xlsx', index=False)
+def check_format_number(format_number):
+    if format_number == 1 or format_number == 2 or format_number == 3:
+        return True
+
+    return False
+
+
+def show_result_check_format_number(result_check_format_number, format_number):
+    if result_check_format_number is True:
+        print(f"You choosed {format_number}")
     else:
-        item_data.to_csv('./export/data.csv', index=False)
-        item_data.to_excel('./export/data.xlsx', index=False)
+        print("This choise doesn't exist. Please, try again.")
+        exit()
+
+
+def get_file_name():
+    file_name = str(input("Please, enter the file name: "))
+
+    return file_name
+
+
+def check_file_name(file_name):
+    if file_name.isspace():
+        print("The file name is empty.")
+        exit()
+    if os.path.exists(f"./export/{file_name}.csv") or os.path.exists(f"./export/{file_name}.xlsx"):
+        print("The file with that name already exists.")
+        exit()
+
+
+def export(item_data, format_number, file_name):
+    if format_number == 1:
+        item_data.to_csv(f"./export/{file_name}.csv", index=False)
+    if format_number == 2:
+        item_data.to_excel(f"./export/{file_name}.xlsx", index=False)
+    if format_number == 3:
+        item_data.to_csv(f"./export/{file_name}.csv", index=False)
+        item_data.to_excel(f"./export/{file_name}.xlsx", index=False)
 
     print("The data has been successfully exported")
